171fb14eb9e3bb588ae53f833241165d024aab01
Jonas Kau <jonaskau@climobi064.intern.lise.de>
2023-01-06 13:14:21 +0100

    UPJK-138 Includes basic LoginController

---
+++ spieleplattform-backend/src/main/kotlin/com/example/spieleplattformbackend/LoginController.kt
@@ -0,0 +1,18 @@
+package com.example.spieleplattformbackend
+
+import com.example.spieleplattformbackend.models.LoginData
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
+
+@RestController
+class LoginController {
+    //var random: Random = Random
+
+    @PostMapping("/login", MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE)
+    fun login(@RequestBody loginData: LoginData) : Boolean {
+        return Random.nextBoolean()
+    }
+}
---
+++ spieleplattform-backend/src/main/kotlin/com/example/spieleplattformbackend/models/LoginData.kt
@@ -0,0 +1,4 @@
+package com.example.spieleplattformbackend.models
+
+class LoginData(var username: String, var password: String) {
+}
