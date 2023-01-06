c9942e0179597c1b1cf6a2957d05f7f5b9dbf6c1
Jonas Kau <jonaskau@climobi064.intern.lise.de>
2023-01-06 11:16:53 +0100

    Includes a basic backend project with Kotlin and Spring

---
+++ spieleplattform-backend/src/main/kotlin/com/example/spieleplattformbackend/SpieleplattformBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spieleplattformbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpieleplattformBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpieleplattformBackendApplication>(*args)
+}
---
+++ spieleplattform-backend/src/test/kotlin/com/example/spieleplattformbackend/SpieleplattformBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spieleplattformbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpieleplattformBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
