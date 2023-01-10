9cebf64d70c5c6c59477dc97c4c216a2735784c7
jonaskau <jonas.kau@lise.de>
2023-01-10 17:00:57 +0100

    UPJK-146 add react frontend app with typescript

--- spieleplattform-frontend/src/App.tsx
+++ spieleplattform-frontend/src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Link, Route, Routes} from "react-router-dom";
+import GameLayout from "./pages/Game/GameLayout";
+import Game from "./pages/Game/Game";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/game/new">New Game</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/game" element={<GameLayout/>}>
+            <Route path=":id" element={<Game/>}/>
+          </Route>
+        </Routes>
+      </>
   );
 }
 
