4b0b867ffcc8801e75c7110607a3036ea4e1edd9
jonaskau <jonas.kau@lise.de>
2023-01-10 13:03:07 +0100

    UPJK-144: Switches from Date to LocalDate

--- spieleplattform-backend/src/main/kotlin/com/example/spieleplattformbackend/models/Game.kt
+++ spieleplattform-backend/src/main/kotlin/com/example/spieleplattformbackend/models/Game.kt
@@ -1,14 +1,14 @@
 package com.example.spieleplattformbackend.models
 
 import jakarta.persistence.*
-import java.util.*
+import java.time.LocalDate
 
 @Entity
 class Game(
     @Column(nullable = false)
     var name: String,
     @Column(nullable = true)
-    var releaseDate: Date,
+    var releaseDate: LocalDate,
     @Column(nullable = true)
     var developer: String,
     @OneToMany(mappedBy = "game", fetch = FetchType.EAGER)
--- spieleplattform-backend/src/main/kotlin/com/example/spieleplattformbackend/services/GameService.kt
+++ spieleplattform-backend/src/main/kotlin/com/example/spieleplattformbackend/services/GameService.kt
@@ -6,13 +6,13 @@ import com.example.spieleplattformbackend.repositories.GameRepository
 import com.example.spieleplattformbackend.repositories.RatingRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.util.*
+import java.time.LocalDate
 
 @Service
 class GameService(@Autowired val gameRepository: GameRepository, @Autowired val ratingRepository: RatingRepository) {
     fun insertExampleGames() {
         //minecraft
-        var minecraft = Game("Minecraft", Date(111, 10, 18), "Mojang Studios")
+        val minecraft = Game("Minecraft", LocalDate.of(2011, 11, 18), "Mojang Studios")
         minecraft.ratings = listOf(
             Rating(5, "Best Game ever", minecraft),
             Rating(4, "Good, but only Blocks", minecraft),
@@ -20,29 +20,29 @@ class GameService(@Autowired val gameRepository: GameRepository, @Autowired val 
         )
         gameRepository.save(minecraft)
         //sims
-        var sims = Game("Die Sims 4", Date(114, 8, 2), "Maxis")
+        val sims = Game("Die Sims 4", LocalDate.of(2014, 12, 18), "Maxis")
         sims.ratings = listOf(
             Rating(5, "I love it", sims)
         )
         gameRepository.save(sims)
         //GTA
-        var gta = Game("Grand Theft Auto V", Date(113, 8, 17), "Rockstar North")
+        val gta = Game("Grand Theft Auto V", LocalDate.of(2013, 9, 17), "Rockstar North")
         gta.ratings = listOf(
             Rating(5, "Awesome game, you can do whatever you want!", gta),
             Rating(3, "Good game, but to much violence", gta)
         )
         gameRepository.save(gta)
         //SuperMario
-        var superMario = Game("Super Mario Bros.", Date(85, 8, 13), "Nintendo Research")
+        val superMario = Game("Super Mario Bros.", LocalDate.of(1985, 9, 13), "Nintendo Research")
         superMario.ratings = listOf(
             Rating(5, "I love this game", superMario)
         )
         gameRepository.save(superMario)
         //Tetris
-        var tetris = Game("Tetris", Date(84, 5, 6), "Alexei Paschitnow")
+        val tetris = Game("Tetris", LocalDate.of(1984, 6, 6), "Alexei Paschitnow")
         gameRepository.save(tetris)
 
-        var games = arrayOf(minecraft, sims, gta, superMario, tetris)
+        val games = arrayOf(minecraft, sims, gta, superMario, tetris)
         for (game in games) {
             for (rating in game.ratings) {
                 ratingRepository.save(rating)
@@ -51,7 +51,7 @@ class GameService(@Autowired val gameRepository: GameRepository, @Autowired val 
     }
 
     fun printGameWithName(name: String) {
-        var game = gameRepository.findGameByName(name) ?: return
+        val game = gameRepository.findGameByName(name) ?: return
 
         println("${game.name}, ${game.developer}, ${game.releaseDate}")
         println("Ratings: ")
