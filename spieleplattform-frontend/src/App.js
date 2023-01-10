a4e9e744ca0d0084addb5252f925456d68eb81a8
jonaskau <jonas.kau@lise.de>
2023-01-10 15:36:59 +0100

    UPJK-146 Adds Frontend-App

---
+++ spieleplattform-frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {Link, Route, Routes } from "react-router-dom";
+import GameLayout from "./pages/Game/GameLayout";
+import Game from "./pages/Game/Game";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+      <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/game/new">Game</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/game" element={<GameLayout />}>
+            <Route path=":id" element={<Game />} />
+          </Route>
+        </Routes>
+      </>
+  );
+}
+
+export default App;
