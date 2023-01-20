7f003df5bac35b8d0e2d43cd4e72d40a6be8fabb
jonaskau <jonas.kau@lise.de>
2023-01-20 09:59:40 +0100

    UPJK-47: Adds Tests for GameConsoleController, GameConsoleService and GameRepository

--- spieleplattform-backend/src/test/kotlin/com/example/spieleplattformbackend/gameConsole/GameConsoleControllerTest.kt
+++ spieleplattform-backend/src/test/kotlin/com/example/spieleplattformbackend/gameConsole/GameConsoleControllerTest.kt
@@ -1,3 +1,37 @@
 package com.example.spieleplattformbackend.gameConsole
 
-class GameConsoleControllerTest
+import com.example.spieleplattformbackend.game.GameService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@WebMvcTest
+class GameConsoleControllerTest(@Autowired val mockMvc: MockMvc) {
+    @MockkBean
+    lateinit var gameConsoleService: GameConsoleService
+
+    @MockkBean
+    lateinit var gameService: GameService
+
+    val gameConsole = GameConsole("Xbox")
+
+    @Test
+    fun when_gameConsoles_withoutParameters() {
+        //given
+        every { gameConsoleService.getGameConsoles() } returns mutableListOf(gameConsole)
+
+        //when//then
+        mockMvc.get("/gameConsoles")
+            .andExpect {
+                status { isOk() }
+                content { contentType(MediaType.APPLICATION_JSON) }
+                MockMvcResultMatchers.jsonPath("$[0].name").value("Xbox")
+            }
+    }
+}
