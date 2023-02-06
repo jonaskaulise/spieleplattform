fc09631ed0de6f75d8e1584232a6b501cba79d48
jonaskau <jonas.kau@lise.de>
2023-02-06 18:34:12 +0100

    UPJK-57: Includes add-game-form in the frontend

--- spieleplattform-frontend/src/components/Rating/Rating.ts
+++ spieleplattform-frontend/src/components/Rating/Rating.ts
@@ -1,10 +1,10 @@
 export default interface Rating {
-    graphicRating: number;
-    soundRating: number;
-    addictionRating: number;
-    actionRating: number;
+    graphicRating: number
+    soundRating: number
+    addictionRating: number
+    actionRating: number
 
-    username: string;
-    comment: string;
-    id: number;
+    username: string
+    comment: string
+    id: number
 }
